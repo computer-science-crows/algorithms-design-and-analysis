bf2185eeff92ed5e7c7c14e232965438440c9183
LRiera24 <laura.riera2412@gmail.com>
2023-03-31 21:56:07 -0400

    tester & generator okkkk :+1:

--- baseball/src/app/generator.py
+++ baseball/src/app/generator.py
@@ -1,29 +1,44 @@
 import numpy as np
 import os
 import random
-from solutions.backtrack_solution import backtrack_solution
+# from solutions.backtrack_solution import backtrack_solution
 import json
-from visualiser.visualiser import Visualiser as vs
-
-def generator(n, k, p, samples=1):
-
-    i = 0   
-
-    while i < samples:  
-    
-        a=[random.randint(0,10) for i in range(n)]    
-        s= np.random.randint(low=0,high=10,size=(n,p+k))
-        solution = backtrack_solution(n,p,k,a,s)
-        vs.make_animation("backtrack_solution.gif", delay=2)
-        i+=1
-
-        #save_input(n,k,p,a,s,solution)  
-
-        return n,p,k,a,s,solution 
-          
+from tools import save_data
+# from visualiser.visualiser import Visualiser as vs
 
 
-#def save_input(n,k,p,a,s,backtrack_sol):
+def generator(n=None, k=None, p=None, samples=1):
+    i = 0
+
+    while i < samples:
+        if n == None:
+            n = random.randint(0, 20)
+        if k == None:
+            k = random.randint(0, 20)
+        if p == None:
+            p = random.randint(0, 20)
+
+        a = [random.randint(0, 10) for i in range(n)]
+        s = np.random.randint(low=0, high=10, size=(n, p+k))
+        s = s.tolist()
+        solution = 0
+        # solution = backtrack_solution(n,p,k,a,s)
+        # vs.make_animation("backtrack_solution.gif", delay=2)
+        i += 1
+
+        data = {"n": n, "p": p, "k": k, "a": a, "s": s,
+                "optimal_solution": solution}
+
+        save_data(data, "/test_cases.json")
+
+        return n, p, k, a, s, solution
+
+
+print(generator())
+
+
+# save_test_case(1, 2, 3, 4, 5, 6)
+# def save_input(n,k,p,a,s,backtrack_sol):
 #
 #    cwd = os.getcwd()
 #    cwd += "/baseball/json"
@@ -36,14 +51,3 @@ def generator(n, k, p, samples=1):
 #
 #    with open(cwd+"/test_cases.json", "w") as write_it:
 #            json.dump(t, write_it)
-
-        
-
-
-
-
-   
-    
-
-    
-
---
+++ baseball/src/app/tools.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+def save_data(data: dict, path: str):
+    cwd = os.getcwd()
+    cwd += "/baseball/json" + path
+
+    save = []
+
+    try:
+        with open(cwd, "r") as read_it:
+            save = json.load(read_it)
+
+        save.append(data)
+
+        with open(cwd, "w") as write_it:
+            json.dump(save, write_it)
+    except:
+        save.append(data)
+
+        with open(cwd, "w") as write_it:
+            json.dump(save, write_it)
+
+
+def Memoize():
+    pass
