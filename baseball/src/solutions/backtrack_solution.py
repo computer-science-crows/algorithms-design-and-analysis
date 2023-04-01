7cf802ed8fad4876c08de9b8877bb1b936dafbb2
LRiera24 <laura.riera2412@gmail.com>
2023-04-01 15:38:00 -0400

    build graph from input, report draft update

--- baseball/src/solutions/backtrack_solution.py
+++ baseball/src/solutions/backtrack_solution.py
@@ -1,41 +1,23 @@
-from visualiser.visualiser import Visualiser as vs
+def backtrack_solution(n, m, a, s):
 
-
-def backtrack_solution(n,m,a,s):
-    
     candidates = [False for i in range(n)]
-    
+
     answer = []
 
-    _backtrack_solution(0,candidates,0,a,s, answer, m)
+    _backtrack_solution(0, candidates, 0, a, s, answer, m)
 
     return max(answer)
 
-@vs(node_properties_kwargs={"shape":"record", "color":"#f57542", "style":"filled", "fillcolor":"grey"})
-def _backtrack_solution(max,candidates, count, a, s, answer,m):
-    
+
+def _backtrack_solution(max, candidates, count, a, s, answer, m):
+
     if count == m:
         answer.append(max)
-        return      
+        return
 
     for j in range(len(candidates)):
-        if not candidates[j]:            
-            candidates[j]=True
-            _backtrack_solution(max + s[j][count] + a[j],candidates, count + 1, a, s, answer,m)
-            candidates[j]=False
-
-    
-
-    
-
-
-
-                
-
-
-
-
-    
-
-    
-
+        if not candidates[j]:
+            candidates[j] = True
+            _backtrack_solution(
+                max + s[j][count] + a[j], candidates, count + 1, a, s, answer, m)
+            candidates[j] = False
