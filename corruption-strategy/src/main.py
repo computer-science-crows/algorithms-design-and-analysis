20e4821bd9ac42713d633b23ee69ccfea20f9bf6
marie0501 <mariedelvallereyes01@gmail.com>
2023-05-17 13:27:46 -0400

    docs and backtrack

--- corruption-strategy/src/main.py
+++ corruption-strategy/src/main.py
@@ -8,7 +8,7 @@ from solutions.backtrack_solution_relaxed import backtrack_solution_relaxed
 # Uncomment lines 12, 15, 16 and 19 as you wish to excecute the desired functions :)
 
 # Generates the number of test cases specified and adds them to test_cases.json
-generator(samples=5)
+#generator(samples=5)
 
 # Test solution for all tests in test_cases.json
 # tester(test_ff)
@@ -24,3 +24,35 @@ generator(samples=5)
 # sol, val = backtrack_solution_relaxed(n, m, a, w)
 # print(f"solucion backtrack {sol}")
 # print(f'valor backtrack {val}')
+
+
+n = 4
+m = 3
+a = [53, 61, 22, 48]
+w = [[3, 2, 83], [2, 4, 17], [3, 1, 90]]
+c, r, p = backtrack_solution_relaxed(n, m, a, w)
+print(f'cities: {c}')
+print(f'roads: {r}')
+print(f'profit: {p}')
+
+
+# "n": 5, "m": 4, "a": [10, 67, 31, 12, 62], "w": [[3, 4, 21], [4, 2, 98], [5, 3, 29], [1, 5, 39]], "elapsed_time": 0.000774383544921875, "cities": [1, 3, 4, 5], "roads": [2, 0, 1], "optimal_value": 33
+n = 5
+m = 4
+a = [10, 67, 31, 12, 62]
+w = [[3, 4, 21], [4, 2, 98], [5, 3, 29], [1, 5, 39]]
+c, r, p = backtrack_solution_relaxed(n, m, a, w)
+print(f'cities: {c}')
+print(f'roads: {r}')
+print(f'profit: {p}')
+
+#"n": 5, "m": 4, "a": [50, 51, 12, 31, 23], "w": [[1, 4, 79], [2, 1, 9], [5, 4, 70], [3, 4, 71]], "elapsed_time": 0.0009076595306396484, "cities": [1, 2, 3, 4, 5], "roads": [1, 0, 3, 2], "optimal_value": 62
+
+n = 5
+m = 4
+a = [50, 51, 12, 31, 23]
+w = [[1, 4, 79], [2, 1, 9], [5, 4, 70], [3, 4, 71]]
+c, r, p = backtrack_solution_relaxed(n, m, a, w)
+print(f'cities: {c}')
+print(f'roads: {r}')
+print(f'profit: {p}')
--- corruption-strategy/src/solutions/backtrack_solution_relaxed.py
+++ corruption-strategy/src/solutions/backtrack_solution_relaxed.py
@@ -4,7 +4,7 @@ import numpy as np
 def backtrack_solution_relaxed(n, m, a, w):
 
     # boolean array to mark selected roads
-    cities = [[-1 for i in range(n)] for j in range(n)]
+    roads = [False for i in range(m)]
 
     # list of best assigment of candidates and position
     best_solution = []
@@ -12,7 +12,7 @@ def backtrack_solution_relaxed(n, m, a, w):
     # list of solutions
     answer = []
 
-    _backtrack_solution_relaxed(cities, best_solution, 0, a, w, answer, m)
+    _backtrack_solution_relaxed(roads, best_solution, 0, a, w, answer, m)
 
     value = max(answer)
     solution = best_solution[answer.index(value)]
@@ -20,12 +20,12 @@ def backtrack_solution_relaxed(n, m, a, w):
     return solution[0], solution[1], value
 
 
-def _backtrack_solution_relaxed(cities, best_solution, count, a, w, answer, m):
+def _backtrack_solution_relaxed(roads, best_solution, count, a, w, answer, m):
 
     # base case
     if count <= m:
         current_cost, current_cities_selection, current_road_selection = cost(
-            cities, a, w, len(a))
+            roads, a, w, len(w))
         answer.append(current_cost)
         best_solution.append(
             [np.copy(current_cities_selection).tolist(), np.copy(current_road_selection).tolist()])
@@ -37,42 +37,40 @@ def _backtrack_solution_relaxed(cities, best_solution, count, a, w, answer, m):
     for i in range(m):
         city_1, city_2, road_cost = w[i]
 
-        if cities[city_1-1][city_2-1] < 0 and city_1 != city_2:
-            cities[city_1-1][city_2-1] = count
-            cities[city_2-1][city_1-1] = count
-            _backtrack_solution_relaxed(cities, best_solution,
+        if not roads[i]:
+            roads[i] = True
+            _backtrack_solution_relaxed(roads, best_solution,
                                         count + 1, a, w, answer, m)
-            cities[city_1-1][city_2-1] = -1
-            cities[city_2-1][city_1-1] = -1
+            roads[i]= False
+            
+            
 
 
-def cost(cities, a, w, n):
+def cost(roads, a, w, m):
 
     solution_cost = 0
 
     mark = [False for i in range(len(a))]
-    mark_cities = [[False for i in range(n)] for i in range(n)]
-
+   
     selected_roads = []
     selected_cities = set()
 
-    for i in range(n):
-        for j in range(n):
-            road_cost = 0
-            if not mark_cities[i][j] and cities[i][j] > -1:
-                mark_cities[i][j] = True
-                mark_cities[j][i] = True
-                city_1, city_2, road = w[cities[i][j]]
-                road_cost += road
-                if not mark[i]:
-                    road_cost -= a[i]
-                    mark[i] = True
-                if not mark[j]:
-                    road_cost -= a[j]
-                    mark[j] = True
-                solution_cost += road_cost
-                selected_roads.append(cities[i][j])
-                selected_cities.add(i+1)
-                selected_cities.add(j+1)
+
+    for i in range(m):
+        road_cost = 0
+        if roads[i]:
+            city_1, city_2, road = w[i]
+            road_cost += road
+            if not mark[city_1-1]:
+                road_cost -= a[city_1-1]
+                mark[city_1-1] = True
+            if not mark[city_2-1]:
+                road_cost -= a[city_2-1]
+                mark[city_2-1] = True
+            solution_cost += road_cost
+            selected_roads.append(i+1)
+            selected_cities.add(city_1)
+            selected_cities.add(city_2)
+
 
     return solution_cost, list(selected_cities), selected_roads
